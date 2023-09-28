1ca0d184776e43a8aad7836e07e1597001d13a27
Lucho Romero <go.romerolae@gmail.com>
2023-09-27 21:07:07 -0500

    added reduce

---
+++ reduceAndMap/scriptReduce.js
@@ -0,0 +1,12 @@
+function countAllStars(galaxies){
+    let totalStars = galaxies.reduce(function(acc, stars){
+        return acc + stars;
+    })
+    return totalStars;
+}
+
+const galaxies = [ 10, 3, 2, 1, 6, 1, 9, 2];
+
+let result = countAllStars(galaxies)
+
+console.log(result)
